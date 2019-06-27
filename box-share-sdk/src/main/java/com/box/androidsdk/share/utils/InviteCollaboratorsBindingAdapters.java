4946f51ccd3ba05ae651c256f1322a3ea9a1adb9
myothiha09 <myothiha1997@gmail.com>
2019-06-27 11:02:31 -0700

    minor changes to lambda

--- box-share-sdk/src/main/java/com/box/androidsdk/share/utils/InviteCollaboratorsBindingAdapters.java
+++ box-share-sdk/src/main/java/com/box/androidsdk/share/utils/InviteCollaboratorsBindingAdapters.java
@@ -15,29 +15,23 @@ public class InviteCollaboratorsBindingAdapters {
 
     @BindingAdapter(value = {"personalMessageTextView", "addPersonalMessageButton", "bottomDivider"})
     public static void onEmptyAndUnfocused(EditText view, View v1, View v2, View v3) {
-        view.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus && ((EditText)v).getText().toString().isEmpty()) {
-                    v1.setVisibility(View.GONE);
-                    v2.setVisibility(View.VISIBLE);
-                    v3.setVisibility(View.GONE);
-                    view.setVisibility(View.GONE);
-                }
+        view.setOnFocusChangeListener((v, hasFocus) -> {
+            if (!hasFocus && ((EditText)v).getText().toString().isEmpty()) {
+                v1.setVisibility(View.GONE);
+                v2.setVisibility(View.VISIBLE);
+                v3.setVisibility(View.GONE);
+                view.setVisibility(View.GONE);
             }
         });
     }
     @BindingAdapter(value = {"personalMessageEditText", "personalMessageTextView", "bottomDivider"})
     public static void onAddPersonalMessageBottom(Button view, View v1, View v2, View v3) {
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v1.setVisibility(View.VISIBLE);
-                v2.setVisibility(View.VISIBLE);
-                v3.setVisibility(View.VISIBLE);
-                view.setVisibility(View.GONE);
-                v1.requestFocus();
-            }
+        view.setOnClickListener(v -> {
+            v1.setVisibility(View.VISIBLE);
+            v2.setVisibility(View.VISIBLE);
+            v3.setVisibility(View.VISIBLE);
+            view.setVisibility(View.GONE);
+            v1.requestFocus();
         });
     }
 
